61da1094a2c31b7120a47aa25145f17a813eb228
IAMDAGOOAT <gopinathanaar@gmail.com>
2025-07-26 14:53:37 +0800

    function

--- functions.py
+++ functions.py
@@ -2,3 +2,9 @@ def my_functions():
     print("This is a function")
     
 my_functions()
+
+def my_function(name,place):
+    print(name,place)
+    
+my_function("krish","Paris")
+my_function("raj","Mumbai")
